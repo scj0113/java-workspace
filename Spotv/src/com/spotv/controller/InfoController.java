9f16485af3613b0652502c580e93838344b0e509
scj0113 <scj0113@naver.com>
2023-06-13 17:49:21 +0900

    20230613

--- Spotv/src/com/spotv/controller/InfoController.java
+++ Spotv/src/com/spotv/controller/InfoController.java
@@ -14,13 +14,42 @@ public class InfoController {
 	
 	// R
 	
+    public void viewPlayerInfo() {                     // 선수 정보 보기
+		
+    }
+	
+	public void viewTeamInfo() {                       // 팀 정보 보기
+		
+	}
+	
+	
+	
 	
 	
 	// U
 	
 	
+    public void updataPlayerInfo() {                     // 선수 정보 수정
+		
+    }
+	
+	public void updataTeamInfo() {                       // 팀 정보 수정
+		
+	}
+	
+	
+	
+	
 	
 	// D
+	
+	public void deletePlayerInfo() {                     // 선수 정보 삭제
+		
+    }
+	
+	public void deleteTeamInfo() {                       // 팀 정보 삭제
+		
+	}
 	
 	
 }
--- Spotv/src/com/spotv/impl/InfoControllerImpl.java
+++ Spotv/src/com/spotv/impl/InfoControllerImpl.java
@@ -5,4 +5,12 @@ public interface InfoControllerImpl {
 	
 	public void addPlayerInfo();
 	public void addTeamInfo();
+	public void viewPlayerInfo();
+	public void viewTeamInfo();
+	public void updataPlayerInfo();
+    public void updataTeamInfo();
+    public void deletePlayerInfo();
+    public void deleteTeamInfo();
+ 
+    
 }
